8c152eb06a98e181c1b59e40b5461710c6f7b594
Steven <ekczile@gmail.com>
2018-10-15 10:47:16 -0400

    2nd pass

--- num_rom_conversion.rb
+++ num_rom_conversion.rb
@@ -1,7 +1,14 @@
 class Integer
 
     def romanizer()
+        numerals = {
+            "I" => 1
+        }
+        x = self
         roman = ""
+        numerals.each do |key, value|
+            roman << key 
+        end
         roman
     end
 end
