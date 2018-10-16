5848b5dd018a372e9fb83423bf9f1eae47879b5b
Steven <ekczile@gmail.com>
2018-10-16 17:50:17 -0400

    400 fails

--- rom_num_test.rb
+++ rom_num_test.rb
@@ -39,12 +39,12 @@ class TestNumberConversion < Minitest::Test
         assert_equal(125, "CXXV".de_romanizer())
         assert_equal(195, "CXCV".de_romanizer())
     end
-    def test_that_500_equals_D
+    def test_that_500_equals_
+        assert_equal(400, "CD".de_romanizer())
         assert_equal(500, "D".de_romanizer())
         assert_equal(545, "DXLV".de_romanizer())
         assert_equal(555, "DLV".de_romanizer())
         assert_equal(595, "DXCV".de_romanizer())
         assert_equal(599, "DXCVIIII".de_romanizer())
-
     end
 end
