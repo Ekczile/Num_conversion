d7563cad2760163efda8cd63f64102adafe74d72
Steven <ekczile@gmail.com>
2018-10-16 17:23:07 -0400

    49 passes

--- rom_num_test.rb
+++ rom_num_test.rb
@@ -30,5 +30,6 @@ class TestNumberConversion < Minitest::Test
         assert_equal(50, "L".de_romanizer())
         assert_equal(45, "XLV".de_romanizer())
         assert_equal(48, "XLVIII".de_romanizer())
+        assert_equal(49, "XLVIIII".de_romanizer())
     end
 end
